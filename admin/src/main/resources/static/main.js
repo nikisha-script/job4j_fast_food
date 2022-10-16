dcb1894865c23b880e4735b6b50fe17bcf590e1a
nikisha-script <nikisha_script@mail.ru>
2022-10-17 01:21:42 +0300

    1. Вёрстка #3

--- admin/src/main/resources/static/main.js
+++ admin/src/main/resources/static/main.js
@@ -1,4 +1,8 @@
-angular.module('market-front', []).controller('indexController', function ($scope, $http, $rootScope) {
+const app = angular.module('market-front', []);
 
+app.controller('indexController', function ($scope, $http, $rootScope) {
+    $scope.foo = function () {
+        alert("Hello");
+    }
 });
 
