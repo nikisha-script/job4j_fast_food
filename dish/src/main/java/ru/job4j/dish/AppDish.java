3d33798622345f0ca923728afe07651df0fbee85
Nikishin Danil <nikisha_script@mail.ru>
2023-01-09 14:49:08 +0300

    #upd

--- dish/src/main/java/ru/job4j/dish/AppDish.java
+++ dish/src/main/java/ru/job4j/dish/AppDish.java
@@ -3,6 +3,9 @@ package ru.job4j.dish;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
 @SpringBootApplication
 public class AppDish {
 
--- dish/src/main/java/ru/job4j/dish/config/Config.java
+++ dish/src/main/java/ru/job4j/dish/config/Config.java
@@ -3,6 +3,7 @@ package ru.job4j.dish.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
